20abfe93a71c1c691afc42397def7338602e39e9
Jacob Butfiloski <jacobbutfiloski@gmail.com>
2018-10-05 12:55:03 -0400

    Added init controls to switch modes
    Jake Butfiloski

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpAutonomous.java
@@ -1,43 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.Main.Vector3;
 
+@TeleOp(name="Test Autonomous", group="Linear Opmode")
 public class OpAutonomous extends OpMode
 {
-    Vector3 v3 = new Vector3(1.0f, 2.0f, 3.1f);
-
-
-
-
-
-
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+    private DcMotor armMotor = null;
+
+    private Servo armServo = null;
+    private final double _maxArmServoPos = 0.0;
+    private final double _minArmServoPos = 1.0;
+    private double armServoPosition = (_maxArmServoPos - _minArmServoPos);
+    private boolean armClosed = false;
+
+    private boolean mode1Enabled = false;
 
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
+        //Motors
+        leftMotor  = hardwareMap.get(DcMotor.class, "left_motor");
+        rightMotor = hardwareMap.get(DcMotor.class, "right_motor");
+        armMotor   = hardwareMap.get(DcMotor.class, "arm_motor");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        //Servos
+        armServo   = hardwareMap.get(Servo.class, "servo_arm");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -47,7 +51,18 @@ public class OpAutonomous extends OpMode
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
     @Override
-    public void init_loop() {
+    public void init_loop()
+    {
+        if(gamepad1.dpad_up) //Mode One
+        {
+            mode1Enabled = true;
+        }
+
+        if(gamepad1.back) //reset button
+        {
+            mode1Enabled = false;
+        }
+
     }
 
     /*
@@ -55,40 +70,38 @@ public class OpAutonomous extends OpMode
      */
     @Override
     public void start() {
+        telemetry.addData("Enabled Modes", "Mode 1: " + mode1Enabled);
         runtime.reset();
     }
 
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
+    boolean configured = false;
+    double startTime = 0.0;
     @Override
     public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        if(!configured)
+        {
+            startTime = runtime.time();
+        }
+        if(mode1Enabled)
+        {
+            telemetry.addData("Status", "Mode 1");
+            while(startTime + 5.0 > runtime.time())
+            {
+                leftMotor.setPower(1);
+                rightMotor.setPower(-1);
+            }
+        } else if(!mode1Enabled)
+        {
+            while(startTime + 5.0 > runtime.time())
+            {
+                leftMotor.setPower(-1);
+                rightMotor.setPower(1);
+            }
+        }
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
-
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
 
     /*
@@ -96,5 +109,19 @@ public class OpAutonomous extends OpMode
      */
     @Override
     public void stop() {
+    }
+
+
+
+    //Other Autonomous Calls
+    void ConsoleLog(String message)
+    {
+        telemetry.addData("Console Log", message);
+    }
+
+    void ConsoleError(Exception error)
+    {
+        String errorString = error.toString();
+        telemetry.addData("Console Error", errorString);
     }
 }
